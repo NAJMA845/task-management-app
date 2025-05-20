193f103021088b1c43e1710970b9e09fb363e101
M.M.F.Najma <s92082845@ousl.lk>
2025-05-20 09:58:19 +0530

    Setting Up React App + Tailwind CSS v4 Setup

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-const App ={} => {
-    return {
-        <div>App</div>
-
-    }
+const App =() =>{
+  return (
+    <div className='text-3xl text-rose-600'>Apps</div>
+  )
 }
 export default App
